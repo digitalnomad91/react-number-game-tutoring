73c4939491663d7ed7f46847e04db17ef98afc90
Andrew Corbin <andrew.c.corbin@gmail.com>
2022-01-16 21:08:33 -0600

    testing bug

--- src/App.js
+++ src/App.js
@@ -149,6 +149,7 @@ function App() {
             </div>
         </div>
         <GameInfo
+            key="gameInfo"
             resetGame={resetGame}
             gameStatus={gameStatus}
             elapsedTime={elapsedTime}
